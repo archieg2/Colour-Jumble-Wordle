d059732cd68a4e462b98353aed3b37d9a38a8353
archieg2 <96123651+archieg2@users.noreply.github.com>
2024-02-22 15:13:51 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+function wordEntered(){
+    let word = document.getElementById('inputWord').value
+    if (word.length != 5){
+         alert('Invalid Entry')
+    }else{
+        if (validWords.includes(word)){
+            alert('hooray')
+        }else{
+            alert('Not in Word List')
+        }
+    }
+    
+}
